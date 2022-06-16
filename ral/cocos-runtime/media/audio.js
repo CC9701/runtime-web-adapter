5986ce929c01a6cc621fcf9e4baf8dbed9bf6f72
cc <chencong_0111@163.com>
2022-06-16 17:13:45 +0800

    [carModel] 适配车机项目的问题

--- ral/cocos-runtime/media/audio.js
+++ ral/cocos-runtime/media/audio.js
@@ -10,4 +10,21 @@ _UTIL.exportTo("createInnerAudioContext", _rt, ral, function () {
             return _CREATE_INNER_AUDIO_CONTEXT(_rt.AudioEngine);
         };
     }
+}, function () {
+    let ctx = ral.createInnerAudioContext();
+    let prototype = ctx.__proto__.constructor.prototype;
+    /**
+     * 适配刚 setSrc 完, 先 seek(0) 再 play 会导致的卡顿问题
+    */
+    const _weakMap = new WeakMap();
+    let oldSeek = prototype.seek;
+    prototype.seek = function (position) {
+        if (this.paused) {
+            // playing 状态下不用再使用 play 方法 seek 就会生效, 所以仅 stopped 和 paused 状态时保存
+            if (this.currentTime && this.currentTime > 0 && position === 0) {
+                return;
+            }
+        }
+        oldSeek.call(this, position);
+    }
 });
